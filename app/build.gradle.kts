24b1cb8169d3584f1272831fb5541154714f77b0
byldh <byldh124@gmail.com>
2024-02-02 08:07:35 +0900

    proguard 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.moondroid.imagepuzzle"
         minSdk = 24
         targetSdk = 33
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -41,16 +41,16 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.activity:activity-ktx:1.8.2")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
--- app/src/main/java/com/moondroid/imagepuzzle/data/Response.kt
+++ app/src/main/java/com/moondroid/imagepuzzle/data/Response.kt
@@ -1,11 +1,15 @@
 package com.moondroid.imagepuzzle.data
 
+import androidx.annotation.Keep
+
+@Keep
 data class Response(
     val code : Int = 0,
     val message: String = "",
     val result: List<String> = emptyList()
 )
 
+@Keep
 data class SimpleResponse(
     val code: Int = 0,
     val message: String = ""
